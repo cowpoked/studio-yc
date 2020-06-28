ac466db0709edf64af0c4fe99e4d452f3ef3fd5a
cowpoked <deathpod@gmail.com>
2020-06-28 19:15:41 -0400

    Updating Instagram link in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,13 +5,13 @@ export default () => (
   <div>
     <h2 className="taCenter">
       Follow Corri{' '}
-      <a href="https://instagram.com/thrivegoldcoast/">@thrivegoldcoast</a>
+      <a href="https://instagram.com/cwillismusic/">@cwillismusic</a>
     </h2>
     <br />
     <footer className="footer">
       <div className="container taCenter">
         <span>
-          © Copyright {new Date().getFullYear()} All rights reserved. Crafted by the Willises
+          © Copyright {new Date().getFullYear()} All rights reserved.
         </span>
       </div>
     </footer>
